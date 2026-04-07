be7411035339f1219522dfb8eb4a173567cfc2f7
venkateswararaohari1 <venkateswararaohari1@gmail.com>
2026-04-07 11:47:12 +0530

    Fix Render deployment crash

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const Database = require('better-sqlite3');
 const path = require('path');
 const bcrypt = require('bcrypt');
 
-const dbPath = path.resolve(__dirname, '../../database/ecommerce.db');
+const dbPath = path.resolve(__dirname, 'ecommerce.db');
 const db = new Database(dbPath, { verbose: console.log });
 
 // Initialize database schema
